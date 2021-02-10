b58a4db286fc9c05c6e343e2f640b667b87af533
Bruno Hradec <39918438+bhradec@users.noreply.github.com>
2021-02-11 00:05:04 +0100

    dodan logout korisnika

--- src/app/main-navigation/main-navigation.component.ts
+++ src/app/main-navigation/main-navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
     selector: 'app-main-navigation',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./main-navigation.component.css']
 })
 export class MainNavigationComponent implements OnInit {
-    constructor() { }
+    constructor(private authService: AuthService) { }
+    
     ngOnInit() { }
+
+    logout() {
+        this.authService.deauthenticateUser();
+    }
 }
