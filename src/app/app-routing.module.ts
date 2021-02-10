2ef1fd6b2914fa145fe26d07401ffc0f026441bd
Bruno Hradec <39918438+bhradec@users.noreply.github.com>
2021-02-10 23:54:01 +0100

    dodana vertikalna glavna navigacija

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GameLibraryComponent } from './game-library/game-library.component';
 import { GameProfileComponent } from './game-profile/game-profile.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -8,12 +9,25 @@ import { AuthGuard } from './shared/other/auth-guard';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const routes: Routes = [
-    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
-    { path: 'userProfile', component: UserProfileComponent, canActivate: [AuthGuard] },
-    { path: 'gameProfile', component: GameProfileComponent, canActivate: [AuthGuard] },
-    { path: 'gameAuthorProfile', component: GameProfileComponent, canActivate: [AuthGuard] }
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: '',
+        component: HomeComponent,
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'gameLibrary', component: GameLibraryComponent, canActivate: [AuthGuard] },
+            { path: 'userProfile', component: UserProfileComponent, canActivate: [AuthGuard] },
+            { path: 'gameProfile', component: GameProfileComponent, canActivate: [AuthGuard] },
+            { path: 'gameAuthorProfile', component: GameProfileComponent, canActivate: [AuthGuard] }
+        ]
+    }
 ];
 
 @NgModule({
