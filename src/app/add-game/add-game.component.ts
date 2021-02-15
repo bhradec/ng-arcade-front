c202500d594b6452efa03ab4c887a2887474df18
Bruno Hradec <39918438+bhradec@users.noreply.github.com>
2021-02-15 13:21:56 +0100

    ispravci na add game komponenti

--- src/app/add-game/add-game.component.ts
+++ src/app/add-game/add-game.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { BehaviorSubject, Subscription } from 'rxjs';
 import { GameAuthor } from '../shared/models/game-author.model';
@@ -17,7 +17,7 @@ export class AddGameComponent implements OnInit {
     gameAuthorsChangeSubject: BehaviorSubject<GameAuthor[]>;
     gameAuthorsChangeSubscription: Subscription;
     gameAuthors: GameAuthor[] = [];
-    gameImage: any;
+    image: any;
 
     constructor(
         private formBuilder: FormBuilder,
@@ -27,12 +27,12 @@ export class AddGameComponent implements OnInit {
 
     ngOnInit() {
         this.addGameForm = this.formBuilder.group({
-            name: new FormControl("", Validators.required),
-            link: new FormControl("", Validators.required),
-            sourceLink: new FormControl("", Validators.required),
-            image: new FormControl("", Validators.required),
-            description: new FormControl("", Validators.required),
-            gameAuthorId: new FormControl("", Validators.required)
+            nameInput: new FormControl("", Validators.required),
+            linkInput: new FormControl("", Validators.required),
+            sourceLinkInput: new FormControl("", Validators.required),
+            imageInput: new FormControl("", Validators.required),
+            descriptionInput: new FormControl("", Validators.required),
+            gameAuthorIdInput: new FormControl("", Validators.required)
         });
 
         this.gameAuthorsChangeSubject = this.gameAuthorService.getGameAuthors();
@@ -42,28 +42,28 @@ export class AddGameComponent implements OnInit {
             });
     }
 
-    get name() {
-        return this.addGameForm.get('name');
-    }
-    
-    get link() {
-        return this.addGameForm.get('link');
+    get nameInput() {
+        return this.addGameForm.get("nameInput");
     }
 
-    get sourceLink() {
-        return this.addGameForm.get('sourceLink');
+    get linkInput() {
+        return this.addGameForm.get("linkInput");
     }
 
-    get image() {
-        return this.addGameForm.get('image');
+    get sourceLinkInput() {
+        return this.addGameForm.get("sourceLinkInput");
     }
 
-    get description() {
-        return this.addGameForm.get('description');
+    get imageInput() {
+        return this.addGameForm.get("imageInput");
     }
 
-    get gameAuthorId() {
-        return this.addGameForm.get('gameAuthorId');
+    get descriptionInput() {
+        return this.addGameForm.get("descriptionInput");
+    }
+
+    get gameAuthorIdInput() {
+        return this.addGameForm.get("gameAuthorIdInput");
     }
 
     isInputInvalid(input) {
@@ -72,21 +72,21 @@ export class AddGameComponent implements OnInit {
 
     onFileChange(event) {
         if (event.target.files.length > 0) {
-            this.gameImage = event.target.files[0];
+            this.image = event.target.files[0];
         }
     }
 
     onSubmit() {
         if (this.addGameForm.invalid) {
-            this.addGameErrorMessage = 'Plesase enter all required data.';            
+            this.addGameErrorMessage = "Plesase enter all required data.";
         } else {
             let formData = new FormData();
-            formData.append("name", this.name.value);
-            formData.append("link", this.link.value);
-            formData.append("sourceLink", this.sourceLink.value);
-            formData.append("image", this.gameImage);
-            formData.append("description", this.description.value);
-            formData.append("gameAuthorId", this.gameAuthorId.value);
+            formData.append("name", this.nameInput.value);
+            formData.append("link", this.linkInput.value);
+            formData.append("sourceLink", this.sourceLinkInput.value);
+            formData.append("image", this.image);
+            formData.append("description", this.descriptionInput.value);
+            formData.append("gameAuthorId", this.gameAuthorIdInput.value);
             this.gameService.addGame(formData);
             this.router.navigate(["/manageGames"]);
         }
