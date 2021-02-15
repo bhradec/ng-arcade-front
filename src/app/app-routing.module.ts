3312a94d7c2ed489051553db4fedd07183c396f3
Bruno Hradec <39918438+bhradec@users.noreply.github.com>
2021-02-15 16:59:49 +0100

    ispravci na game author profile, game tile i user profile komponentama

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { AddGameAuthorComponent } from './add-game-author/add-game-author.component';
 import { AddGameComponent } from './add-game/add-game.component';
 import { AdministrationComponent } from './administration/administration.component';
 import { EditGameAuthorComponent } from './edit-game-author/edit-game-author.component';
 import { EditGameComponent } from './edit-game/edit-game.component';
+import { GameAuthorProfileComponent } from './game-author-profile/game-author-profile.component';
 import { GameLibraryComponent } from './game-library/game-library.component';
 import { GameProfileComponent } from './game-profile/game-profile.component';
 import { HomeComponent } from './home/home.component';
@@ -33,7 +34,7 @@ const routes: Routes = [
             { path: "", component: GameLibraryComponent, canActivate: [AuthGuard] },
             { path: "userProfile", component: UserProfileComponent, canActivate: [AuthGuard] },
             { path: "gameProfile/:id", component: GameProfileComponent, canActivate: [AuthGuard] },
-            { path: "gameAuthorProfile/:id", component: GameProfileComponent, canActivate: [AuthGuard] },
+            { path: "gameAuthorProfile/:id", component: GameAuthorProfileComponent, canActivate: [AuthGuard] },
             { path: "administration", component: AdministrationComponent, canActivate: [AuthGuard, AdminGuard] },
             { path: "manageGames", component: ManageGamesComponent, canActivate: [AuthGuard, AdminGuard] },
             { path: "manageGameAuthors", component: ManageGameAuthorsComponent, canActivate: [AuthGuard, AdminGuard] },
--- src/app/game-author-profile/game-author-profile.component.ts
+++ src/app/game-author-profile/game-author-profile.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { GameAuthor } from '../shared/models/game-author.model';
+import { Game } from '../shared/models/game.model';
+import { GameAuthorService } from '../shared/services/game-author.service';
+import { GameService } from '../shared/services/game.service';
 
 @Component({
     selector: 'app-game-author-profile',
@@ -6,6 +12,43 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./game-author-profile.component.css'],
 })
 export class GameAuthorProfileComponent implements OnInit {
-    constructor() { }
-    ngOnInit() { }
+    gameAuthorsChangeSubject: BehaviorSubject<GameAuthor[]>;
+    gamesChangeSubject: BehaviorSubject<Game[]>;
+    gameAuthorsChangeSubscription: Subscription;
+    gamesChangeSubscription: Subscription;
+    gameAuthors: GameAuthor;
+    gameAuthor: GameAuthor;
+    games: Game[] = [];
+    errorMessage: string;
+
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private gameAuthorService: GameAuthorService, 
+        private gameService: GameService) { }
+        
+    ngOnInit() {
+        let routeParams = this.activatedRoute.snapshot.paramMap;
+        let id = Number(routeParams.get("id"));
+
+        this.gameAuthorsChangeSubject = this.gameAuthorService.getGameAuthors();
+        this.gameAuthorsChangeSubscription = this.gameAuthorsChangeSubject
+            .subscribe((res: GameAuthor[]) => {
+                this.gameAuthor = res.find((gameAuthor) => {
+                    return gameAuthor.id = id;
+                });
+                if (!this.gameAuthor) {
+                    this.errorMessage = `Game author with the id ${id} does not exist.`;
+                } else {
+                    this.errorMessage = "";
+                }
+            });
+
+        this.gamesChangeSubject = this.gameService.getGames();
+        this.gamesChangeSubscription = this.gamesChangeSubject
+            .subscribe((res: Game[]) => {
+                this.games = res.filter((game) => {
+                    return game.gameAuthorId == id; 
+                });
+            });
+    }
 }
