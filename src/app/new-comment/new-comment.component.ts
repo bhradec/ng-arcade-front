33e2ecfaa79643f02271f53572514c6216951218
Bruno Hradec <39918438+bhradec@users.noreply.github.com>
2021-02-15 15:25:33 +0100

    ispravci na new comment komponenti

--- src/app/new-comment/new-comment.component.ts
+++ src/app/new-comment/new-comment.component.ts
@@ -22,23 +22,27 @@ export class NewCommentComponent implements OnInit {
 
     ngOnInit(): void {
         this.newCommentForm = this.formBuilder.group({
-            "username": new FormControl({ value: this.user.username, disabled: true }),
-            "comment": new FormControl("", Validators.required)
+            "usernameInput": new FormControl({ value: this.user.username, disabled: true }),
+            "commentInput": new FormControl("", Validators.required)
         });
     }
 
-    get username() { return this.newCommentForm.get("username"); }
-    get comment() { return this.newCommentForm.get("comment"); }
+    get usernameInput() { return this.newCommentForm.get("usernameInput"); }
+    get commentInput() { return this.newCommentForm.get("commentInput"); }
+
+    isInputInvalid(input) {
+        return input.invalid && (input.dirty || input.touched);
+    }
 
     onSubmit(): void {
         this.newComment = new Comment();
 
-        this.newComment.comment = this.comment.value;
+        this.newComment.comment = this.commentInput.value;
         this.newComment.timestamp = new Date();
         this.newComment.userId = this.user.id;
         this.newComment.gameId = this.gameId;
         this.newComment.username = this.user.username;
-        
+
         this.commentService.addComment(this.newComment);
     }
 }
